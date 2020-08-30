88cce7d476e16b4a2ff4f9fdb97d84df4508f93e
stevenks17 <stevenks17@gmail.com>
2020-08-30 12:34:51 -0400

    Event/Attendee Association

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
   has_many :conferences
   has_many :speakers, through: :conferences
+  has_many :attendees, through: :conferences
 
   validates :title, presence: true, length: { maximum: 255, minimum: 5 }
   validates :starts_at, presence: true, date: { after: Proc.new{ Time.now } }
