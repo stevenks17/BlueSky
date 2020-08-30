df874db9ea8e10974956630d533239fbe4814f6b
stevenks17 <stevenks17@gmail.com>
2020-08-30 12:32:22 -0400

    Attendees Endpoint Logic/Pagy Pagination

--- app/controllers/reports/events_controller.rb
+++ app/controllers/reports/events_controller.rb
@@ -4,6 +4,15 @@ module Reports
       @event = Event.find events_params[:event_id]
     end
 
+    def attendees
+      @event = Event.find events_params[:event_id]
+      @attendees = @event.attendees
+      @pagy, @attendees = pagy(@event.attendees, items: 500)
+
+    end
+
+
+
     private
 
     def events_params
